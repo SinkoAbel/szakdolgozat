76edd3fed1e44587e12bc104bb3471785a03035e
sinkoabel <sinkoabel@gmail.com>
2023-08-09 20:58:34 +0200

    feat: initialized missing packages, created a template folder structure for future reference.

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {ChakraProvider} from '@chakra-ui/react';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import Navbar from "./components/Navbar";
+import Home from "./screens/Home/Home";
 
 function App() {
   return (
-      <ChakraProvider>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      </ChakraProvider>
+    <ChakraProvider>
+        <BrowserRouter>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+            </Routes>
+        </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
