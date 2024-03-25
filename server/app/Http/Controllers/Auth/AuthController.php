d48788d3dbbf96f7adc9ec4e57ba4958626614bf
Sinkó <sinkoabel@gmail.com>
2024-03-25 09:59:38 +0100

    fix: cors issues has been resolved

--- server/app/Http/Controllers/Auth/AuthController.php
+++ server/app/Http/Controllers/Auth/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
     {
     }
 
-    public function index(LoginRequest $request): JsonResponse
+    public function login(LoginRequest $request): JsonResponse
     {
         return response()->json(
             $this->service->login($request),
